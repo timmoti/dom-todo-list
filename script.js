9f92e332fc2453e966b3a4b6334bb5dc28e2e5b0
TimothyTan <timmoti@gmail.com>
2018-09-22 08:08:56 +0800

    Simplified concept

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const tasks = [
-	'Enter a new task above',
-	'Click to mark done',
-	'Clear completed task from screen'
-];
+const tasks = [];
 
 const ul = document.querySelector('ul');
 const input = document.querySelector('input');
@@ -11,35 +7,46 @@ const button = document.querySelector('button');
 const removeElements = (elms) => {
 	[...elms].forEach(el => el.remove())
 }
-button.addEventListener('click', function() {
-	removeElements(document.querySelectorAll('.done'))
-})
 
 // button.addEventListener('click', function() {
-// 	let li = document.createElement('li');
-// 	li.textContent = input.value;
-// 	ul.appendChild(li);
-// 	input.value = '';
-// })
-
+	// 	let li = document.createElement('li');
+	// 	li.textContent = input.value;
+	// 	ul.appendChild(li);
+	// 	input.value = '';
+	// })
+	
 input.addEventListener('keypress', function(e) {
 	if (e.charCode === 13) {
 		let li = document.createElement('li');
-	li.textContent = input.value;
-	ul.appendChild(li);
-	input.value = '';
+		li.textContent = input.value;
+		ul.appendChild(li);
+		input.value = '';
+
+		// let checkbox = document.createElement('input');
+		// checkbox.type = "checkbox";
+		// checkbox.value = 1;
+		// checkbox.name = "todo[]"
+		// li.appendChild(checkbox);
+
 	}
 })
 
-for (let task of tasks) {
-	let li = document.createElement('li');
-	li.textContent = task;
-	ul.appendChild(li);
-}
 
+
+
+// for (let task of tasks) {
+// 	let li = document.createElement('li');
+// 	li.textContent = task;
+// 	ul.appendChild(li);
+// 	console.log(typeof li);
+// }
+	
 ul.addEventListener('click', function(e) {
 	if (e.target && e.target.nodeName === 'LI') {
 		e.target.classList.toggle("done");
 	}
 })
-
+	
+button.addEventListener('click', function() {
+	removeElements(document.querySelectorAll('.done'))
+})
