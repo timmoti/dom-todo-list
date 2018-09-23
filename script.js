ed8a07c26f48d15b7c8f9400ddb035939f1f23f5
TimothyTan <timmoti@gmail.com>
2018-09-23 21:59:12 +0800

    Styled better

--- script.js
+++ script.js
@@ -32,6 +32,30 @@ input.addEventListener('keypress', function(e) {
 	}
 })
 
+// Credits: http://blog.vishalon.net/index.php/javascript-getting-and-setting-caret-position-in-textarea/
+
+function setCaretPosition(ctrl, pos) {
+	// Modern browsers
+	if (ctrl.setSelectionRange) {
+	  ctrl.focus();
+	  ctrl.setSelectionRange(pos, pos);
+	
+	// IE8 and below
+	} else if (ctrl.createTextRange) {
+	  var range = ctrl.createTextRange();
+	  range.collapse(true);
+	  range.moveEnd('character', pos);
+	  range.moveStart('character', pos);
+	  range.select();
+	}
+  }
+
+// Set the cursor position of the "#test-input" element to the end when the page loads
+let input = document.querySelector('input');
+
+setCaretPosition(input, input.value.length);
+
+
 
 
 
